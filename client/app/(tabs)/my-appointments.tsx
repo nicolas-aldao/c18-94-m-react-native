9d90f87639f945d3918d112f26135fa5aee51b0e
Nicolás Aldao <nicolasaldao@live.com>
2024-05-24 10:23:27 -0300

    comments and unused code deleted

--- client/app/(tabs)/my-appointments.tsx
+++ client/app/(tabs)/my-appointments.tsx
@@ -1,14 +1,8 @@
-// import { useContext } from "react";
 import { StyleSheet } from "react-native";
-import { Link } from "expo-router";
-// import { MedConnectContext } from "@/context";
-import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { TopBar } from "@/components/molecules/TopBar";
 
 export default function SpecialtyScreen() {
-    // const { user } = useContext(MedConnectContext);
-
     return (
         <>
             <TopBar title="Mis Turnos" backArrow />
@@ -24,9 +18,5 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         padding: 20,
-    },
-    link: {
-        marginTop: 15,
-        paddingVertical: 15,
     }
 });
