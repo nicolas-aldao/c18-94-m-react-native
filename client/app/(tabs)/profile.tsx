40d13f4afc8fed704bcc8aa5905ec98669cf7290
Nicolás Aldao <nicolasaldao@live.com>
2024-05-24 10:18:26 -0300

    unused code deleted

--- client/app/(tabs)/profile.tsx
+++ client/app/(tabs)/profile.tsx
@@ -24,9 +24,5 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         padding: 20,
-    },
-    link: {
-        marginTop: 15,
-        paddingVertical: 15,
-    },
+    }
 });
