38c0b28393443095194ce92ec5dcb94774f26559
Nicolás Aldao <nicolasaldao@live.com>
2024-06-14 09:24:10 -0300

    videocall token updated

--- server/src/routes/videocallRouter.js
+++ server/src/routes/videocallRouter.js
@@ -9,7 +9,7 @@ videocallRouter.get("/", async (req, res, next) => {
       appID: "9e59ec728ba649dcb247e112747bccea",
       channel: "call",
       token:
-        "007eJxTYFh783LiHXFfzX0x6SaKYRdNREOEDc4vZTzF21JjLhl6M0CBwTLV1DI12dzIIinRzMQyJTnJyMQ81dDQyNzEPCk5OTWxbUp2WkMgI0PB30esjAwQCOKzMCQn5uQwMAAAsGMeDA==",
+        "007eJxTYIgvEJ1cUrg6V53ryKqvW0oWyjUsTr99NSvL5ZvI+eZnbg0KDJapppapyeZGFkmJZiaWKclJRibmqYaGRuYm5knJyamJqmY5aQ2BjAzpb2QZGRkgEMRnYUhOzMlhYAAAbBcfmg==",
     };
     req.data = videocallInfo;
     req.statusCode = 200;
